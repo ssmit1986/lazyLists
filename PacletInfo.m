285167060804efd654e7eb151130959ae69e6b2c
Sjoerd Smit <sjoerds@wolfram.com>
2018-09-18 12:58:54 +0100

    Make lazyList package with example notebook

---
+++ PacletInfo.m
@@ -0,0 +1,17 @@
+(* Paclet Info File *)
+
+(* created 2018/09/18*)
+
+Paclet[
+    Name -> "lazyLists",
+    Version -> "1",
+    MathematicaVersion -> "11.3",
+    Description -> "Mathematica implementation of Haskell-style lazy lists.",
+    Creator -> "Sjoerd Smit",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English"}
+        }
+]
+
+
