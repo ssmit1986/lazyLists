f106d916f2d4d080a75274bb5ab3e87a85e46806
Sjoerd Smit <sjoerds@wolfram.com>
2019-07-18 10:45:16 +0100

    move pacletinfo file

--- lazyLists/PacletInfo.m
+++ lazyLists/PacletInfo.m
@@ -1,11 +1,11 @@
 (* Paclet Info File *)
 
-(* created 2018/09/18*)
+(* created 2019/07/18*)
 
 Paclet[
     Name -> "lazyLists",
     Version -> "1",
-    MathematicaVersion -> "11.3",
+    MathematicaVersion -> "12",
     Description -> "Mathematica implementation of Haskell-style lazy lists.",
     Creator -> "Sjoerd Smit",
     Extensions -> 
